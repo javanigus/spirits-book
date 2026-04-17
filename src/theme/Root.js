ede72341c65b0b2ac015800f0e347d5078916118
Abdullah Yahya <abdullah747@gmail.com>
2026-04-17 11:07:09 -0700

    Fixed Arabic RTL issues on mobile

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -5,12 +5,6 @@ export default function Root({ children }) {
   const location = useLocation();
 
   useEffect(() => {
-    const isArabic = location.pathname.includes('/arabic/');
-
-    document.documentElement.setAttribute('dir', isArabic ? 'rtl' : 'ltr');
-    document.documentElement.setAttribute('lang', isArabic ? 'ar' : 'en');
-    document.body.setAttribute('dir', isArabic ? 'rtl' : 'ltr');
-
     if (location.hash) {
       const id = location.hash.substring(1);
 
@@ -22,12 +16,6 @@ export default function Root({ children }) {
         }
       }, 100);
     }
-
-    return () => {
-      document.documentElement.setAttribute('dir', 'ltr');
-      document.documentElement.setAttribute('lang', 'en');
-      document.body.setAttribute('dir', 'ltr');
-    };
   }, [location.pathname, location.hash]);
 
   return <>{children}</>;
